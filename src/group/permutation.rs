8d32ebd18febd607f02e3fcc7e841cdd339d3171
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-08-15 10:50:57 +0200

    Correctly display permutations

--- src/group/permutation.rs
+++ src/group/permutation.rs
@@ -2,6 +2,9 @@
 //! this forms a group.
 
 use std::collections::HashMap;
+use std::collections::HashSet;
+use std::fmt;
+use std::fmt::Display;
 use super::{GroupElement, GroupAction};
 
 /// A permutation of there set 0..n for a suitable choice of n.
@@ -60,6 +63,50 @@ impl GroupAction for Permutation {
     fn act_on(&self, original: &u64) -> u64 {
         self.images.get(&original).unwrap_or(&original).clone()
     }
+}
+
+impl Display for Permutation {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let cycles: Vec<Vec<u64>> = cycles(self.n, &self.images);
+        if cycles.len() > 0 {
+            for cycle in cycles {
+                let representations: Vec<String> =
+                    cycle
+                    .into_iter()
+                    .map(|element| format!("{}", element))
+                    .collect();
+                let representation: String = representations.join(" ");
+                write!(f, "(")?;
+                write!(f, "{}", representation)?;
+                write!(f, ")")?
+            }
+            write!(f, "")
+        } else {
+            write!(f, "Id")
+        }
+    }   
+}
+
+fn cycles(n: usize, images: &HashMap<u64, u64>) -> Vec<Vec<u64>> {
+    let mut cycles = vec!();
+    let mut visited = HashSet::new();
+    for i in 0..n {
+        let original = i as u64;
+        if !visited.contains(&original) {
+            visited.insert(original.clone());
+            let mut cycle = vec!(original.clone());
+            let mut image = images.get(&original).unwrap_or(&original).clone();
+            while !visited.contains(&image) {
+                visited.insert(image.clone());
+                cycle.push(image.clone());
+                image = images.get(&image).unwrap_or(&image).clone();
+            }
+            if cycle.len() > 1 {
+                cycles.push(cycle);
+            }
+        }
+    }
+    cycles
 }
 
 #[cfg(test)]
@@ -136,5 +183,24 @@ mod tests {
         assert_eq!(permutation.act_on(&0u64), 1u64);
         assert_eq!(permutation.act_on(&1u64), 2u64);
         assert_eq!(permutation.act_on(&2u64), 0u64);
+    }
+
+    #[test]
+    fn permutation_should_display_correctly() {
+        let mut identity_images = HashMap::new();
+        identity_images.insert(0u64, 0u64);
+        identity_images.insert(1u64, 1u64);
+        let identity = Permutation::new(identity_images);
+
+        let mut permutation_images = HashMap::new();
+        permutation_images.insert(0u64, 1u64);
+        permutation_images.insert(1u64, 2u64);
+        permutation_images.insert(2u64, 0u64);
+        permutation_images.insert(3u64, 4u64);
+        permutation_images.insert(4u64, 3u64);
+        let permutation = Permutation::new(permutation_images);
+
+        assert_eq!("Id", format!("{}", identity));
+        assert_eq!("(0 1 2)(3 4)", format!("{}", permutation));
     }
 }
