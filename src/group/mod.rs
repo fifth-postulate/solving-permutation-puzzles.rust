79b8c5cdb094cd304ccdaa1bb9a7aff4ea7b132c
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-08-16 14:02:13 +0200

    Create an actual group

--- src/group/mod.rs
+++ src/group/mod.rs
@@ -33,24 +33,85 @@ pub trait GroupAction {
     fn act_on(&self, element: &Self::Domain) -> Self::Domain;
 }
 
+/// The actual group.
+pub struct Group<Domain, G>
+    where Domain: Eq + Hash + Clone, G: GroupElement + GroupAction<Domain=Domain> {
+    levels: Vec<BaseStrongGeneratorLevel<Domain, G>>,
+}
 
-struct BaseStrongGeneratorLevel<Domain, G> where G: GroupElement + GroupAction<Domain=Domain> {
+impl<Domain, G> Group<Domain, G>
+    where Domain: Eq + Hash + Clone, G: GroupElement + GroupAction<Domain=Domain> {
+    /// Creates a group with a given set of generators on a certain gset.
+    pub fn new(gset: Vec<Domain>, generators: Vec<G>) -> Group<Domain, G> {
+        let mut levels = vec!();
+        let mut gs = generators;
+        while gs.len() > 0 {
+            let base: Domain = find_base(&gset, &gs).expect("generators should move something");
+            let (level, stabilizers) = BaseStrongGeneratorLevel::new(base, &gs);
+            levels.push(level);
+            gs = stabilizers;
+        }
+        Group { levels: levels }
+    }
+
+    /// The order of the group, i.e. the number of elements this group has.
+    pub fn size(&self) -> usize {
+        self.levels
+            .iter()
+            .fold(1usize, |acc, ref level| acc * level.transversal.len() )
+    }
+
+    /// Determine if a group element is a member of this group.
+    pub fn is_member(&self, element: G) -> bool {
+        let candidate = self.strip(element);
+        candidate.is_identity()
+    }
+
+    fn strip(&self, element: G) -> G {
+        let mut candidate = element;
+        for level in &self.levels {
+            if level.has_transversal_for(&candidate) {
+                let transversal = level.transversal_for(&candidate).expect("should have transversal");
+                let inverse = transversal.inverse();
+                candidate = candidate.times(&inverse);
+            } else {
+                break;
+            }
+        }
+        candidate
+    }
+}
+
+fn find_base<Domain, G>(gset: &Vec<Domain>, generators: &Vec<G>) -> Option<Domain>
+    where Domain: Eq + Hash + Clone, G: GroupElement + GroupAction<Domain=Domain> {
+    for original in gset {
+        for generator in generators {
+            let image = generator.act_on(&original);
+            if &image != original {
+                return Some(image.clone())
+            }
+        }
+    }
+    None
+}
+
+struct BaseStrongGeneratorLevel<Domain, G>
+    where Domain: Eq + Hash + Clone, G: GroupElement + GroupAction<Domain=Domain> {
     base: Domain,
-    generators: Vec<G>,
     transversal: HashMap<Domain, G>,
-    stabilizers: Vec<G>,
 }
 
 impl<Domain, G> BaseStrongGeneratorLevel<Domain, G>
     where Domain: Eq + Hash + Clone, G: GroupElement + GroupAction<Domain=Domain> {
-    fn new(base: Domain, generators: Vec<G>) -> BaseStrongGeneratorLevel<Domain, G> {
+    fn new(base: Domain, generators: &Vec<G>) -> (BaseStrongGeneratorLevel<Domain, G>, Vec<G>) {
         let (transversal, stabilizers) = calculate_transversal(base.clone(), &generators);
-        BaseStrongGeneratorLevel {
-            base: base,
-            generators: generators,
-            transversal: transversal,
-            stabilizers: stabilizers,
-        }
+        (
+            BaseStrongGeneratorLevel  {
+                base: base,
+                transversal: transversal,
+            },
+            stabilizers
+        )
     }
 
     fn has_transversal_for(&self, g: &G) -> bool {
@@ -84,7 +145,9 @@ fn calculate_transversal<Domain, G>(base: Domain, generators: &Vec<G>) -> (HashM
                 let to = transversals.get(&element).unwrap();
                 let fro = transversals.get(&image).unwrap().inverse();
                 let stabilizer = to.times(&generator).times(&fro);
-                stabilizers.push(stabilizer);
+                if !stabilizer.is_identity() {
+                    stabilizers.push(stabilizer);
+                }
             }
         }
     }
@@ -95,4 +158,51 @@ fn identity<G>(generators: &Vec<G>) -> G where G: GroupElement {
     let g = generators.get(0).expect("at least one generator");
     let inverse = g.inverse();
     g.times(&inverse)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    use super::permutation::Permutation;
+    use super::*;
+
+    fn d3() -> Group<u64, Permutation> {
+        let mut transposition_images = HashMap::new();
+        transposition_images.insert(0u64, 1u64);
+        transposition_images.insert(1u64, 0u64);
+        transposition_images.insert(2u64, 2u64);
+        let transposition = Permutation::new(transposition_images);
+
+        let mut rotation_images = HashMap::new();
+        rotation_images.insert(0u64, 1u64);
+        rotation_images.insert(1u64, 2u64);
+        rotation_images.insert(2u64, 0u64);
+        let rotation = Permutation::new(rotation_images);
+
+        let gset = vec!(0u64, 1u64, 2u64);
+        let generators = vec!(transposition, rotation);
+
+        Group::new(gset, generators)
+    }
+
+    #[test]
+    fn group_should_have_a_size() {
+        let group = d3();
+
+        assert_eq!(group.size(), 6);
+    }
+
+    #[test]
+    fn group_should_determine_if_an_element_is_a_member() {
+        let mut transposition_images = HashMap::new();
+        transposition_images.insert(0u64, 2u64);
+        transposition_images.insert(1u64, 1u64);
+        transposition_images.insert(2u64, 0u64);
+        let transposition = Permutation::new(transposition_images);
+
+        let group = d3();
+
+        assert!(group.is_member(transposition));
+    }
 }
