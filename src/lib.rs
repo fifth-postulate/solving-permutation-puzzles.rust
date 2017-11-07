f759781da693492f17d7629cb197960defca8790
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-11-07 22:16:30 +0100

    Correctly state that the schreier-sims algorithm is implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![deny(missing_docs)]
 //! A library to make calculations with groups.
 //!
-//! Ultimately we will implement the [Schreier-Sims algorithm](https://en.wikipedia.org/wiki/Schreier%E2%80%93Sims_algorithm).
+//! This implements the [Schreier-Sims algorithm](https://en.wikipedia.org/wiki/Schreier%E2%80%93Sims_algorithm).
 
 pub mod group;
